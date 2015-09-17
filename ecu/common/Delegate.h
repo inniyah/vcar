79999e3a26aba1ab9684770a4329ee9ad9921413
Miriam Ruiz <miriam@debian.org>
2015-09-17 21:13:50 +0200

    Use C++11

--- ecu/common/Delegate.h
+++ ecu/common/Delegate.h
@@ -7,78 +7,48 @@
 
 namespace common {
 
+#include <cstdlib>
+
+// See: http://blog.coldflake.com/posts/C++-delegates-on-steroids/
+
 #if __cplusplus <= 199711L // not a C++11 compliant compiler
-
-// Usage:
-//     A a;
-//     typedef Delegate<int, int> IntDelegate;
-//     IntDelegate d = IntDelegate::fromObjectMethod<A, &A::foo>(&a);
-
-template<typename return_type, typename param_type>
-class Delegate {
-
-	typedef return_type (*Type)(void * callee, param_type);
-
-public:
-
-	Delegate() : fpCallee(NULL), fpCallbackFunction(Delegate::EmptyFunction) { }
-	Delegate(void * callee, Type function) : fpCallee(callee), fpCallbackFunction(function) { }
-
-	bool operator==(const Delegate & other) {
-		return ((fpCallee != other.fpCallee) && (fpCallbackFunction != other.fpCallbackFunction));
-	}
-
-	template <class T, return_type (T::*TMethod)(param_type)>
-	static Delegate fromObjectMethod(T * callee) {
-		Delegate d(callee, &methodCaller<T, TMethod>);
-		return d;
-	}
-
-	return_type operator()(param_type x) const {
-		return (*fpCallbackFunction)(fpCallee, x);
-	}
-
-private:
-
-	void * fpCallee;
-	Type fpCallbackFunction;
-
-	template <class T, return_type (T::*TMethod)(param_type)>
-	static return_type methodCaller(void* callee, param_type x) {
-		T * p = static_cast<T*>(callee);
-		return (p->*TMethod)(x);
-	}
-	static return_type EmptyFunction(void * callee, param_type param) {
-	}
-};
-
-#else // at least a C++11 compliant compiler
+#error At least a C++11 compliant compiler is needed
+#endif
 
 // Usage:
 //     A a;
 //     auto d = Delegate<int, int>::from_function<A, &A::foo>(&a);
 //     auto d2 = Delegate<int, int, int, char>::from_function<A, &A::bar>(&a);
+
 template<typename return_type, typename... params>
 class Delegate {
-
-		typedef return_type (*Type)(void* callee, params...);
-
 public:
+	typedef return_type (*Type)(void* callee, params...);
 
 	Delegate(void * callee, Type function) : fpCallee(callee) , fpCallbackFunction(function) { }
-
-	template <class T, return_type (T::*TMethod)(params...)>
-	static Delegate from_function(T * callee) {
-		Delegate d(callee, &methodCaller<T, TMethod>);
-		return d;
-	}
+	Delegate(const Delegate & d) : fpCallee(d.fpCallee) , fpCallbackFunction(d.fpCallbackFunction) { }
 
 	return_type operator()(params... xs) const {
 		return (*fpCallbackFunction)(fpCallee, xs...);
 	}
 
-private:
+	Delegate & operator=(const Delegate & d) {
+		fpCallee = d.fpCallee;
+		fpCallbackFunction = d.fpCallbackFunction;
+		return *this;
+	}
 
+	bool operator==(const Delegate & d) const {
+		return ((fpCallee == d.fpCallee) && (fpCallbackFunction == d.fpCallbackFunction));
+	}
+
+	template <class T, return_type (T::*TMethod)(params...)>
+	static Delegate fromObjectMethod(T * callee) {
+		Delegate d(callee, &methodCaller<T, TMethod>);
+		return d;
+	}
+
+private:
 	void * fpCallee;
 	Type fpCallbackFunction;
 
@@ -88,8 +58,6 @@ private:
 		return (p->*TMethod)(xs...);
 	}
 };
-
-#endif
 
 } // namespace common
 
